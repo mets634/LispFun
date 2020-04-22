3ac8cb5b1a11e585e9642c20a8127179442fc2c2
mets634 <mets634@gmail.com>
2020-04-22 15:46:04 +0300

    Added loop and closure fibonacci implementations

--- src/fibonacci.lisp
+++ src/fibonacci.lisp
@@ -1,6 +1,6 @@
 (defpackage fibonacci
   (:use :cl)
-  (:export :fibonacci-rec :main))
+  (:export :fibonacci-rec :fibonacci-closure :main))
 (in-package :fibonacci)
 
 (declaim (ftype (function (fixnum) fixnum) fibonacci-rec))
@@ -10,6 +10,27 @@
   (if (< n 2)
       n
       (+ (fibonacci-rec (- n 2)) (fibonacci-rec (- n 1)))))
+
+
+(defun make-fibonacci ()
+  (let ((curr 0) (next 1) temp)
+    (lambda ()
+      (setf temp next)
+      (setf next (+ curr next))
+      (setf curr temp))))
+
+(declaim (ftype (function (fixnum) list) fibonacci-closure))
+(defun fibonacci-closure (n)
+  (let ((fib-gen (make-fibonacci)))
+    (loop repeat n collect (funcall fib-gen))))
+
+(declaim (ftype (function (fixnum) fixnum) fibonacci-loop))
+(defun fibonacci-loop (n)
+  (do ((i 0 (1+ i))
+       (curr 0 next)
+       (next 1 (+ curr next)))
+      ((< n i) curr)))
+
 
 (defun main (argv)
   (print (fibonacci-rec (parse-integer (read-line)))))
